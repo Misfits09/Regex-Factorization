2706777f26c0f5efdd94c61638f68fa25a5f3fc5
Nathan Perriolat <nathanp@theodo.fr>
2022-06-08 12:32:04 +0200

    v1.0.1

--- build/index.js
+++ build/index.js
@@ -4,7 +4,9 @@ exports.simplifyExistingRegex = exports.regexFromNumberArray = void 0;
 var make_tree = function (numbers) {
     var tree = {};
     numbers.forEach(function (nb) {
-        nb.toString().split('').reduce(function (parentNode, digit, index) {
+        nb.toString()
+            .split("")
+            .reduce(function (parentNode, digit, index) {
             if (parentNode[digit] === undefined) {
                 parentNode[digit] = {};
             }
@@ -17,25 +19,31 @@ var make_regex = function (numbers_tree) {
     var regex = "";
     var makeLogic = function (subtree) {
         if (subtree === {})
-            return '';
+            return "";
         else {
             var keys = Object.keys(subtree).map(function (s) { return parseInt(s); });
             keys.sort(function (a, b) { return a - b; });
-            return keys.map(function (n) { return n.toString(); }).map(function (key) {
+            return keys
+                .map(function (n) { return n.toString(); })
+                .map(function (key) {
                 var subregex = makeLogic(subtree[key]);
                 if (subregex.length === 0)
                     return key;
-                else if (!subregex.includes('|'))
+                else if (!subregex.includes("|"))
                     return "".concat(key).concat(subregex);
                 else
                     return "".concat(key, "(").concat(subregex, ")");
-            }).join('|');
+            })
+                .join("|");
         }
     };
     var notGroupedRegex = makeLogic(numbers_tree);
-    return notGroupedRegex.replace(/\((\d\|?)+\)/g, function (match) {
+    return notGroupedRegex.replace(/\((\d\|)+\d\)/g, function (match) {
         var groups = [];
-        match.substring(1, match.length - 1).split('|').reduce(function (groupIndex, n) {
+        match
+            .substring(1, match.length - 1)
+            .split("|")
+            .reduce(function (groupIndex, n) {
             var nbr = parseInt(n);
             if (groupIndex === -1) {
                 groups.push([nbr]);
@@ -50,24 +58,42 @@ var make_regex = function (numbers_tree) {
                 return groupIndex + 1;
             }
         }, -1);
-        return "(".concat(groups.map(function (list) { return list.length === 1 ? list[0].toString() : "[".concat(list[0], "-").concat(list[list.length - 1], "]"); }).join('|'), ")");
+        return "(".concat(groups
+            .map(function (list) {
+            return list.length === 1
+                ? list[0].toString()
+                : "[".concat(list[0], "-").concat(list[list.length - 1], "]");
+        })
+            .join("|"), ")");
     });
 };
 /**
  * Creates a regex from an array of numbers
  */
-var regexFromNumberArray = function (array) { return make_regex(make_tree(array)); };
+var regexFromNumberArray = function (array) {
+    return make_regex(make_tree(array));
+};
 exports.regexFromNumberArray = regexFromNumberArray;
 /**
  * Replace any part of a regex containing ORs with just digits like [a-Z]hello(123|456|32) or a(3|5|6)
  *
  * WARNING The list of numbers must be contained in parenthesis or representing the entire string
  */
-var simplifyExistingRegex = function (regex) { return regex.replace(/^((\d+|(\(\d+\)))\|?)+$/g, function (match) {
-    return (0, exports.regexFromNumberArray)(match.split('|').map(function (s) { return s.replace(/(^\()|(\)$)/g, ''); }).map(function (s) { return parseInt(s); }));
-}).replace(/\(((\d+|(\(\d+\)))\|?)+\)/g, function (match) {
-    match = match.substring(1, match.length - 1);
-    var simplified = (0, exports.regexFromNumberArray)(match.split('|').map(function (s) { return s.replace(/(^\()|(\)$)/g, ''); }).map(function (s) { return parseInt(s); }));
-    return "(".concat(simplified, ")");
-}); };
+var simplifyExistingRegex = function (regex) {
+    return regex
+        .replace(/^((\d+|(\(\d+\)))\|?)+$/g, function (match) {
+        return (0, exports.regexFromNumberArray)(match
+            .split("|")
+            .map(function (s) { return s.replace(/(^\()|(\)$)/g, ""); })
+            .map(function (s) { return parseInt(s); }));
+    })
+        .replace(/\(((\d+|(\(\d+\)))\|?)+\)/g, function (match) {
+        match = match.substring(1, match.length - 1);
+        var simplified = (0, exports.regexFromNumberArray)(match
+            .split("|")
+            .map(function (s) { return s.replace(/(^\()|(\)$)/g, ""); })
+            .map(function (s) { return parseInt(s); }));
+        return "(".concat(simplified, ")");
+    });
+};
 exports.simplifyExistingRegex = simplifyExistingRegex;
